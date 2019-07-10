a70deffb490140608fc9b2db55f57e44779d2e9d
QAtests01 <mprakash@A.local>
2019-07-10 21:18:48 +0100

    automation

---
+++ MegQATest/js/cypress/support/step_definitions/login.js
@@ -0,0 +1,19 @@
+given("the user has the correct credentials", () => {
+  throw new Error("Not implemented");
+});
+
+when("the user enters username", () => {
+  throw new Error("Not implemented");
+});
+
+then("the user enters password", () => {
+  throw new Error("Not implemented");
+});
+
+then("clicks Login", () => {
+  throw new Error("Not implemented");
+});
+
+then("the user is presented with a welcome message", () => {
+  throw new Error("Not implemented");
+});
