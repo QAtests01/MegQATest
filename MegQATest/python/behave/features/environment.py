3361068c7ba75328167fe2b7438ced732ca701c1
QAtests01 <mprakash@A.local>
2019-07-10 21:22:59 +0100

    automation

--- MegQATest/python/behave/features/environment.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-def before_all(context):
-    context.browser = webdriver.Chrome("behave/features/chromedriver.exe")
-
-def after_all(context):
-	context.browser.quit()
